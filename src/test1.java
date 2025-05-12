7f8186c3169c49efd19ac3f2b1a808ed6c891799
Jitendra Pawar <jitendrapawar2@gmail.com>
2025-05-12 12:47:38 +0530

    10th May 2025

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,5 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world This is my first JAVA Program now I become a Automation Tester");
     }
 }
