9a60972bab18263b8d7ee932e8b1c392ecf71762
Jitendra Pawar <jitendrapawar2@gmail.com>
2025-07-06 01:01:41 +0530

    git push code

--- src/ex_01_Java_Basics/HelloWorld.java
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -3,7 +3,7 @@ package ex_01_Java_Basics;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Sai baba");
-        System.out.println("I am Jitendra and I am Master in JAVA");
+        System.out.println("I am Jitendra and I am Master in JAVA1");
     }
 }
 
