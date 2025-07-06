60bebbeffab107ba69f9cbb02801f01bbb39eb13
Jitendra Pawar <jitendrapawar2@gmail.com>
2025-07-06 13:18:28 +0530

    codeTest

--- src/ex_01_Java_Basics/HelloWorld.java
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -2,7 +2,7 @@ package ex_01_Java_Basics;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Sai baba");
+        System.out.println("hello world");
         System.out.println("I am Jitendra and I am Master in JAVA1");
     }
 }
